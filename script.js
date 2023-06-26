0b75674226be0ba61d9b7d109d9e2480298b5cf5
Clyde Mondero <clydereinyelmondero0324@gmail.com>
2023-06-27 06:47:14 +0800

    Created add function

---
+++ script.js
@@ -0,0 +1,14 @@
+//TODO: Add Function
+function add(num1, num2) {
+  let sum = num1 + num2;
+
+  return sum;
+}
+
+let num1 = parseInt(prompt("Enter a number"));
+
+let operator;
+
+let num2 = parseInt(prompt("Enter another number"));
+
+console.log(add(num1, num2));
